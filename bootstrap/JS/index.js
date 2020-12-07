a20967bfcd293ae4ee8d4293b629f832bc202886
Ítalo Conceição <italooliveiradaconceicao@gmail.com>
2020-12-06 23:39:54 -0300

    Primeiro Commit

---
+++ bootstrap/JS/index.js
@@ -0,0 +1,32 @@
+var nome = "Marcelo Eltz";
+var cargo = "CTO - Growdev";
+var nomeHtml = document.getElementById("nome-no-html");
+var cargoHtml = document.getElementById("cargo-no-html");
+var texto1 = document.getElementById("texto-1");
+var texto2 = document.getElementById("texto-2");
+
+function colocarNomeNoHtml(nome){
+nomeHtml.innerHTML = nome;
+}
+
+function colocarCargoNoHtml(cargo){
+cargoHtml.innerHTML = cargo;
+}
+
+function logarNome(){
+    console.log(nome);
+};
+
+function clickNoProjetos(){
+    console.log("clicou no botão projetos");
+    texto2.style.display = "block";
+    texto1.style.display = "none";
+};
+
+function clickNoSobre(){
+    texto2.style.display = "none";
+    texto1.style.display = "block";
+};
+
+colocarNomeNoHtml(nome);
+colocarCargoNoHtml(cargo);
